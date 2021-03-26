aec263db5e3189d4b5a33c396ee2cbc6aabf25cc
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 14:13:28 +0700

    Tugas 15 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+let AB = 8;
+let BC = 6;
+console.log("Panjang AB : "+" "+AB);
+console.log("Panjang BC : "+" "+BC);
+let c = Math.pow(AB,2)+Math.pow(BC,2)
+let AC = Math.sqrt(c)
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah : "+AC+" cm");
